2355fba63afeabb17bf3be898f3cacfb09ffc76c
SpencerBranch <65453198+SpencerBranch@users.noreply.github.com>
2021-05-25 20:42:58 -0400

    Update game.py

--- game.py
+++ game.py
@@ -6,3 +6,17 @@ print("Rock, Paper, Scissors, Shoot!")
 user_choice = input("Please choose on of 'rock', 'paper', 'scissor' ")
 
 print("User Choice: ", user_choice)
+
+if (user_choice == "rock") or (user_choice == "paper") or (user_choice == "scissor"):
+    print("Valid Choice")
+else:
+        print("Not a valid choice")
+        exit()
+
+
+
+
+
+
+
+print("This is the nend of our game.  Please play again")
