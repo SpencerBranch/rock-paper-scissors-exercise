bf99116a2c87c02fad28f762f3f4488acbdaa0f6
SpencerBranch <65453198+SpencerBranch@users.noreply.github.com>
2021-05-27 18:40:20 -0400

    Update game.py

--- game.py
+++ game.py
@@ -1,5 +1,9 @@
 # game.py
+#Import Modules
+import random
 
+
+#Initial Game Heading
 print("Rock, Paper, Scissors, Shoot!")
 
 
@@ -7,6 +11,7 @@ user_choice = input("Please choose on of 'rock', 'paper', 'scissor' ")
 
 print("User Choice: ", user_choice)
 
+#Validate User Choice
 if (user_choice == "rock") or (user_choice == "paper") or (user_choice == "scissor"):
     print("Valid Choice")
 else:
@@ -14,7 +19,20 @@ else:
         exit()
 
 
+#Create Array for possible choices
+arr = ["rock", "paper", "scissor"]
 
+comp = random.choice(arr)
+
+print("The computer chose ", comp)
+
+if user_choice == comp:
+#if (user_choice == "rock" and comp == "rock") or (user_choice == "paper" and comp == "paper") or (user_choice == "scissor" and comp== "scissor"):
+    print("The game ends in a draw")
+elif (user_choice == "rock" and comp == "scissor") or (user_choice == "scissor" and comp == "paper") or (user_choice == "paper" and comp == "rock"):
+    print("Player has won the game")
+else:
+    print("The Computer has won")
 
 
 
