84c0e5ee0b51c3e60a36136faaea2283c30c5252
SpencerBranch <65453198+SpencerBranch@users.noreply.github.com>
2021-05-27 22:35:55 -0400

    Using while loop to validate input

--- game.py
+++ game.py
@@ -14,35 +14,29 @@ print(f"Welcome {PLAYER_NAME} to Rock, Paper, Scissors")
 print("---------")
 
 
-
+#Prompt user to choose and display choice
 user_choice = input("Please choose on of 'rock', 'paper', 'scissor' ")
-
-print("User Choice: ", user_choice)
-
 #Validate User Choice
-if (user_choice == "rock") or (user_choice == "paper") or (user_choice == "scissor"):
-    print("Valid Choice")
-else:
-        print("Not a valid choice")
-        exit()
+#Opted for while loop to allow user to enter multiple values rather than exit program
+while user_choice not in ("rock", "paper", "scissor"):
+    print("You selection is invalid")
+    user_choice = input("Please choose rock paper or scissor ")
+print(f"You have selected {user_choice}")
 
 
 #Create Array for possible choices
 arr = ["rock", "paper", "scissor"]
-
 comp = random.choice(arr)
-
+#Output Computer's choice
 print("The computer chose ", comp)
 
+#Logic for choosing winner
 if user_choice == comp:
-#if (user_choice == "rock" and comp == "rock") or (user_choice == "paper" and comp == "paper") or (user_choice == "scissor" and comp== "scissor"):
     print("The game ends in a draw")
 elif (user_choice == "rock" and comp == "scissor") or (user_choice == "scissor" and comp == "paper") or (user_choice == "paper" and comp == "rock"):
     print("Player has won the game")
 else:
     print("The Computer has won")
-
-
 
 
 print("This is the nend of our game.  Please play again")
