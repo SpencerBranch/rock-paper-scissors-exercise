cd2ca83f5d235ef405cb2e83383c5efee90ff915
SpencerBranch <65453198+SpencerBranch@users.noreply.github.com>
2021-05-27 22:56:23 -0400

    While loop to keep playing

--- game.py
+++ game.py
@@ -13,30 +13,34 @@ print("---------")
 print(f"Welcome {PLAYER_NAME} to Rock, Paper, Scissors")
 print("---------")
 
+#Creating a loop to play
+play_again = "yes"
+while play_again == "yes":
 
-#Prompt user to choose and display choice
-user_choice = input("Please choose on of 'rock', 'paper', 'scissor' ")
-#Validate User Choice
-#Opted for while loop to allow user to enter multiple values rather than exit program
-while user_choice not in ("rock", "paper", "scissor"):
-    print("You selection is invalid")
-    user_choice = input("Please choose rock paper or scissor ")
-print(f"You have selected {user_choice}")
+    #Prompt user to choose and display choice
+    user_choice = input("Please choose on of 'rock', 'paper', 'scissor' ")
+    #Validate User Choice
+    #Opted for while loop to allow user to enter multiple values rather than exit program
+    while user_choice not in ("rock", "paper", "scissor"):
+        print("You selection is invalid")
+        user_choice = input("Please choose rock paper or scissor ")
+    print(f"You have selected {user_choice}")
 
 
-#Create Array for possible choices
-arr = ["rock", "paper", "scissor"]
-comp = random.choice(arr)
-#Output Computer's choice
-print("The computer chose ", comp)
+    #Create Array for possible choices
+    arr = ["rock", "paper", "scissor"]
+    comp = random.choice(arr)
+    #Output Computer's choice
+    print("The computer chose ", comp)
 
-#Logic for choosing winner
-if user_choice == comp:
-    print("The game ends in a draw")
-elif (user_choice == "rock" and comp == "scissor") or (user_choice == "scissor" and comp == "paper") or (user_choice == "paper" and comp == "rock"):
-    print("Player has won the game")
-else:
-    print("The Computer has won")
+    #Logic for choosing winner
+    if user_choice == comp:
+        print("The game ends in a draw")
+    elif (user_choice == "rock" and comp == "scissor") or (user_choice == "scissor" and comp == "paper") or (user_choice == "paper" and comp == "rock"):
+        print("Player has won the game")
+    else:
+        print("The Computer has won")
+    play_again = input("would you like to play again? (yes/no) ")
 
 
 print("Thanks for playing, please play again soon!")
